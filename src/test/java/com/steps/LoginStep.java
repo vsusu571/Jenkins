3fb190cf97bbede1d0b92398247dab067b1d5802
zackw <zackw@10.0.0.169>
2023-11-09 13:12:19 -0500

    test

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -65,7 +65,7 @@ public class LoginStep {
 	public void system_redirects_to_dashboard_pages(String string) {
 		try {
 			System.out.println("aseetion failure...");
-			Assert.assertEquals("java", "not java");
+			//Assert.assertEquals("java", "not java");
 			//Assert.fail();
 		} catch (Exception e) {
 			// TODO: handle exception
